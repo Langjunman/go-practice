196351244451a5e8c66c91fd65a90d9db8690ad5
桾侠 郎 <langjunman@gmail.com>
2022-05-10 16:04:29 +0800

    Initial commit

---
+++ day1/dict.go
@@ -0,0 +1,212 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+)
+
+type DictRequest struct {
+	TransType string `json:"trans_type"`
+	Source    string `json:"source"`
+	UserID    string `json:"user_id"`
+}
+
+type DictResponse struct {
+	Rc   int `json:"rc"`
+	Wiki struct {
+		KnownInLaguages int `json:"known_in_laguages"`
+		Description     struct {
+			Source string      `json:"source"`
+			Target interface{} `json:"target"`
+		} `json:"description"`
+		ID   string `json:"id"`
+		Item struct {
+			Source string `json:"source"`
+			Target string `json:"target"`
+		} `json:"item"`
+		ImageURL  string `json:"image_url"`
+		IsSubject string `json:"is_subject"`
+		Sitelink  string `json:"sitelink"`
+	} `json:"wiki"`
+	Dictionary struct {
+		Prons struct {
+			EnUs string `json:"en-us"`
+			En   string `json:"en"`
+		} `json:"prons"`
+		Explanations []string      `json:"explanations"`
+		Synonym      []string      `json:"synonym"`
+		Antonym      []string      `json:"antonym"`
+		WqxExample   [][]string    `json:"wqx_example"`
+		Entry        string        `json:"entry"`
+		Type         string        `json:"type"`
+		Related      []interface{} `json:"related"`
+		Source       string        `json:"source"`
+	} `json:"dictionary"`
+}
+
+func query(word string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	client := &http.Client{}
+	request := DictRequest{TransType: "en2zh", Source: word}
+	buf, err := json.Marshal(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var data = bytes.NewReader(buf)
+	req, err := http.NewRequest("POST", "https://api.interpreter.caiyunai.com/v1/dict", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("DNT", "1")
+	req.Header.Set("os-version", "")
+	req.Header.Set("sec-ch-ua-mobile", "?0")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36")
+	req.Header.Set("app-name", "xy")
+	req.Header.Set("Content-Type", "application/json;charset=UTF-8")
+	req.Header.Set("Accept", "application/json, text/plain, */*")
+	req.Header.Set("device-id", "")
+	req.Header.Set("os-type", "web")
+	req.Header.Set("X-Authorization", "token:qgemv4jr1y38jyq6vhvi")
+	req.Header.Set("Origin", "https://fanyi.caiyunapp.com")
+	req.Header.Set("Sec-Fetch-Site", "cross-site")
+	req.Header.Set("Sec-Fetch-Mode", "cors")
+	req.Header.Set("Sec-Fetch-Dest", "empty")
+	req.Header.Set("Referer", "https://fanyi.caiyunapp.com/")
+	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9")
+	req.Header.Set("Cookie", "_ym_uid=16456948721020430059; _ym_d=1645694872")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if resp.StatusCode != 200 {
+		log.Fatal("bad StatusCode:", resp.StatusCode, "body", string(bodyText))
+	}
+	var dictResponse DictResponse
+	err = json.Unmarshal(bodyText, &dictResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("----------彩云翻译----------")
+	fmt.Println(word, "UK:", dictResponse.Dictionary.Prons.En, "US:", dictResponse.Dictionary.Prons.EnUs)
+	for _, item := range dictResponse.Dictionary.Explanations {
+		fmt.Println(item)
+	}
+}
+
+type VolcanoRequest struct {
+	Text     string `json:"text"`
+	Language string `json:"language"`
+}
+type VocanoResponse struct {
+	Words []struct {
+		Source  int    `json:"source"`
+		Text    string `json:"text"`
+		PosList []struct {
+			Type      int `json:"type"`
+			Phonetics []struct {
+				Type int    `json:"type"`
+				Text string `json:"text"`
+			} `json:"phonetics"`
+			Explanations []struct {
+				Text     string `json:"text"`
+				Examples []struct {
+					Type      int `json:"type"`
+					Sentences []struct {
+						Text      string `json:"text"`
+						TransText string `json:"trans_text"`
+					} `json:"sentences"`
+				} `json:"examples"`
+				Synonyms []interface{} `json:"synonyms"`
+			} `json:"explanations"`
+			Relevancys []interface{} `json:"relevancys"`
+		} `json:"pos_list"`
+	} `json:"words"`
+	Phrases  []interface{} `json:"phrases"`
+	BaseResp struct {
+		StatusCode    int    `json:"status_code"`
+		StatusMessage string `json:"status_message"`
+	} `json:"base_resp"`
+}
+
+func VolcanoQuery(word string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	client := &http.Client{}
+	request := VolcanoRequest{Text: word, Language: "en"}
+	buf, err := json.Marshal(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var data = bytes.NewReader(buf)
+	req, err := http.NewRequest("POST", "https://translate.volcengine.com/web/dict/match/v1/?msToken=&X-Bogus=DFSzswVLQDaKSvQrSWa/Bl9WX7nP&_signature=_02B4Z6wo00001wYhufgAAIDDhiNDuaj6AI8GIb1AAKP8lT6tRqaiIUCTOHrSxYAWscXoZJ0MJFNTgamey3nA098JpXpMnrKE87tYt5kDVRF6HYBLI3H6F-j8VqM2wpUvKs81h8qOX0Kld8eZ1b", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("authority", "translate.volcengine.com")
+	req.Header.Set("accept", "application/json, text/plain, */*")
+	req.Header.Set("accept-language", "zh-CN,zh;q=0.9,en;q=0.8")
+	req.Header.Set("content-type", "application/json")
+	req.Header.Set("cookie", "x-jupiter-uuid=16521649533224446; i18next=zh-CN; ttcid=5373be7eb4b345c3b3f94ce25dd081b419; tt_scid=RzKAhCBR0xponw7-1gOvDRQIPZsNxBCkJNB6muEiBM6mJ4cBNlocY3lyWH0AkE984028; s_v_web_id=verify_2d397f2fdb8aa123305bcfb96603fdcf; _tea_utm_cache_2018=undefined")
+	req.Header.Set("origin", "https://translate.volcengine.com")
+	req.Header.Set("referer", "https://translate.volcengine.com/translate?category=&home_language=zh&source_language=detect&target_language=zh&text=hello%0A%0A")
+	req.Header.Set("sec-ch-ua", `" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"`)
+	req.Header.Set("sec-ch-ua-mobile", "?0")
+	req.Header.Set("sec-ch-ua-platform", `"Windows"`)
+	req.Header.Set("sec-fetch-dest", "empty")
+	req.Header.Set("sec-fetch-mode", "cors")
+	req.Header.Set("sec-fetch-site", "same-origin")
+	req.Header.Set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if resp.StatusCode != 200 {
+		log.Fatal("bad StatusCode:", resp.StatusCode, "body", string(bodyText))
+	}
+	var volcanoResponse VocanoResponse
+	err = json.Unmarshal(bodyText, &volcanoResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("----------火山翻译----------")
+	for _, word := range volcanoResponse.Words {
+		for _, poslist := range word.PosList {
+			for _, explanation := range poslist.Explanations {
+				fmt.Println(explanation.Text)
+			}
+
+		}
+	}
+}
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, `usage: simpleDict WORD
+example: simpleDict hello
+		`)
+		os.Exit(1)
+	}
+	var wg sync.WaitGroup
+	word := os.Args[1]
+	wg.Add(2)
+	go query(word, &wg)
+	go VolcanoQuery(word, &wg)
+	wg.Wait()
+	return
+
+}
---
+++ day1/guessing_name.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	maxNum := 100
+	rand.Seed(time.Now().UnixNano())
+	secretNumber := rand.Intn(maxNum)
+	//fmt.Println("The secret number is ", secretNumber)
+
+	fmt.Println("Please input your guess")
+	//reader := bufio.NewReader(os.Stdin)
+	for {
+		//input, err := reader.ReadString('\n')
+		var input int
+		_, err := fmt.Scanf("%d\r\n", &input)
+		if err != nil {
+			fmt.Println("An error occured while reading input. Please try again", err)
+			continue
+		}
+		//input = strings.TrimSuffix(input, "\r\n")
+
+		//guess, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Invalid input. Please enter an integer value")
+			continue
+		}
+		fmt.Println("You guess is", input)
+		if input > secretNumber {
+			fmt.Println("Your guess is bigger than the secret number. Please try again")
+		} else if input < secretNumber {
+			fmt.Println("Your guess is smaller than the secret number. Please try again")
+		} else {
+			fmt.Println("Correct, you Legend!")
+			break
+		}
+	}
+}
